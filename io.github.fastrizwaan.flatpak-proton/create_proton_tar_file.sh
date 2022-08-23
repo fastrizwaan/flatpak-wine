87c1d39cbe3aa32ae1ca13a7f7b15c500e9de2b7
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2022-08-23 14:42:51 +0530

    create tar.zst on centos 7

--- io.github.fastrizwaan.flatpak-proton/create_proton_tar_file.sh
+++ io.github.fastrizwaan.flatpak-proton/create_proton_tar_file.sh
@@ -4,4 +4,10 @@ sudo flatpak install flathub --system com.valvesoftware.Steam.CompatibilityTool.
 SRC_DIR=$(dirname $(realpath $0))
 echo "SRC_DIR = $SRC_DIR"
 
-tar -I "zstd -19 -T4" -cvf Proton-GE-7.29.tar.zst -C "/var/lib/flatpak/runtime/com.valvesoftware.Steam.CompatibilityTool.Proton-GE/x86_64/stable/active/files" .
+TAR_FILE="Proton-GE-7.29.tar"
+TAR_ZST="${TAR_FILE}.zst"
+#tar -I "zstd -19 -T4" -cvf Proton-GE-7.29.tar.zst -C "/var/lib/flatpak/runtime/com.valvesoftware.Steam.CompatibilityTool.Proton-GE/x86_64/stable/active/files" .
+
+tar -cvf ${TAR_FILE} -C "/var/lib/flatpak/runtime/com.valvesoftware.Steam.CompatibilityTool.Proton-GE/x86_64/stable/active/files" .
+
+time zstd -19 -T0 -v "${TAR_FILE}" --rm
