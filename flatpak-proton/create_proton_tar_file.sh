6948ead366ce44ad9ac8e95314eb7ba338136ab3
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2022-08-31 19:57:44 +0530

    use updated proton on build

--- flatpak-proton/create_proton_tar_file.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-sudo flatpak install flathub --system com.valvesoftware.Steam.CompatibilityTool.Proton-GE/x86_64/stable
-SRC_DIR=$(dirname $(realpath $0))
-echo "SRC_DIR = $SRC_DIR"
-
-tar -I "zstd -19 -T4" -cvf Proton-GE-7.29.tar.zst -C "/var/lib/flatpak/runtime/com.valvesoftware.Steam.CompatibilityTool.Proton-GE/x86_64/stable/active/files" .
