ae668f49c4395a5ed44bd42cdefdb89a5636cd32
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2022-11-18 03:34:09 +0530

    0.89.7

--- flatpak-proton/install.sh
+++ flatpak-proton/install.sh
@@ -16,7 +16,7 @@ APP_ID="io.github.fastrizwaan.flatpak-proton"
 SHORT_APP_ID="flatpak-proton-ge"
 DATE=$(date +'%Y%m%d')
 
-WINEZGUI_VERSION=0.89.4
+WINEZGUI_VERSION=0.89.7
 
 if [ "${1}" = "sdk" ] ; then
      sudo flatpak --system remote-add --if-not-exists \
--- flatpak-winezgui/install.sh
+++ flatpak-winezgui/install.sh
@@ -15,7 +15,7 @@ APP_ID="io.github.fastrizwaan.WineZGUI"
 SHORT_APP_ID="flatpak-winezgui"
 DATE=$(date +'%Y%m%d')
 
-WINEZGUI_VERSION=0.89.4
+WINEZGUI_VERSION=0.89.7
 
 if [ "${1}" = "sdk" ] ; then
      sudo flatpak --system remote-add --if-not-exists \
