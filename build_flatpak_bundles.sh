4968b5b127d607a345849baa71983d50d3525000
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2023-02-01 01:52:46 +0530

    Prepare 0.91

--- build_flatpak_bundles.sh
+++ build_flatpak_bundles.sh
@@ -2,6 +2,8 @@ flatpak remove -y io.github.fastrizwaan.WineZGUI
 flatpak remove -y io.github.fastrizwaan.flatpak-proton
 flatpak remove -y io.github.fastrizwaan.flatpak-wine5
 flatpak remove -y io.github.fastrizwaan.flatpak-wine7
+flatpak remove -y io.github.fastrizwaan.flatpak-wine8
+
 
 time for i in flatpak-*; do 
 $i/install.sh bundle; done
