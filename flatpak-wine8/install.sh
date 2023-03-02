eba16ae24950300e0d5aeee61ede87f3e11a5b92
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2023-03-02 23:12:28 +0530

    Release 0.93.7 open shell fix

--- flatpak-wine8/install.sh
+++ flatpak-wine8/install.sh
@@ -34,7 +34,7 @@ APP_ID="io.github.fastrizwaan.flatpak-wine8"
 SHORT_APP_ID="flatpak-wine8"
 DATE=$(date +'%Y%m%d')
 
-WINEZGUI_VERSION=0.93.6
+WINEZGUI_VERSION=0.93.7
 BRANCH=22.08
 
 # handle relative path for building
