b8066220d04bb11a78141cbbd3c1b6a58199f5e2
Asif Ali Rizvan <fast.rizwaan@gmail.com>
2023-03-06 10:12:08 +0530

    0.94.5

--- flatpak-wine7/install.sh
+++ flatpak-wine7/install.sh
@@ -34,7 +34,7 @@ APP_ID="io.github.fastrizwaan.flatpak-wine7"
 SHORT_APP_ID="flatpak-wine7"
 DATE=$(date +'%Y%m%d')
 
-WINEZGUI_VERSION=0.94.2
+WINEZGUI_VERSION=0.94.5
 BRANCH=22.08
 
 # handle relative path for building
